7f714591bcbd608a217ba62cef761469614c0bc1
Tejagangoji <124460877+Tejagangoji@users.noreply.github.com>
2023-12-29 18:10:35 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,11 +4,12 @@ const cookieParser = require('cookie-parser');
 
 const mongoose = require('mongoose');
 const app = express();
-// Enable CORS for all routes
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*'); // Allow requests from any origin
+  const allowedOrigin = 'https://curious-platypus-651cab.netlify.app';
+  res.header('Access-Control-Allow-Origin', allowedOrigin);
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.header('Access-Control-Allow-Headers', 'Content-Type');
+  res.header('Access-Control-Allow-Credentials', 'true'); // Allow credentials
   next();
 });
 app.use(cookieParser());
